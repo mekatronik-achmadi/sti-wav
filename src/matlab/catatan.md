a7a481f6e0d7882329b179069f438489446deaff
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-10-21 16:48:19 +0700

    catatan draft

---
+++ src/matlab/catatan.md
@@ -0,0 +1,6 @@
+## Catatan Pengembangan
+
+Berikut adalah porting seluruh class STI menjadi fungsi-fungsi Matlab.
+Seluruh kode sumber Matlab sifatnya masih DRAFT dan perlu perbaikan/pengembangan lebih jauh.
+
+ 
--- src/matlab/test.m
+++ src/matlab/test.m
@@ -1,2 +1,16 @@
-refAudio, refRate = readwav('../speech/eval1.wav')
-degrAudio, degrRate = readwav('../speech/eval1_echo100.wav')
+[refAudio, refRate] = readwav('../speech/eval1.wav');
+[degrAudio, degrRate] = readwav('../speech/eval1_echo100.wav');
+
+stis = stiFromAudio(refAudio, degrAudio, refRate, name='eval1.wav');
+
+fprintf('Test Result: \n');
+
+if abs(stis - 0.63) < 0.002:
+    fprintf('OK\n');
+    return 0
+else:
+    print('FAILED\n');
+    return 1
+end
+
+
